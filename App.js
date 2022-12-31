2790b52b6547bbd66a72393a304dfe8f2d39970d
Kevin Stradtman <webkit.development.systems@gmail.com>
2022-12-31 00:29:14 -0500

    Added an image and linear gradient

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, ImageBackground } from 'react-native';
 import StartGameScreen from './screens/StartGameScreen';
-
+import LinearGradient from 'react-native-linear-gradient';
 
 const App = () => {
   return (
-    <View style={styles.rootScreen}>
-      <StartGameScreen/>
-    </View>  
+    <LinearGradient style={styles.rootScreen} colors={['#ddb52f', '#4e0329']}>
+      <ImageBackground 
+        source={require('./assets/Images/background.png')} 
+        resizeMode="cover" 
+        style={styles.rootScreen}
+        imageStyle={styles.backgroundImage}
+      >
+        <StartGameScreen/>
+      </ImageBackground>
+    </LinearGradient>  
   );
 };
 
 const styles = StyleSheet.create({
   rootScreen: {
     flex: 1,
-    backgroundColor: '#ddb52f'
+  },
+  backgroundImage: {
+    opacity: 0.25
   }
 });
 
